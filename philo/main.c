503d90a0f4307f361171dbb4746e0408101061dd
kmeeseek <beaverdi147@gmail.com>
2021-07-24 15:38:24 +0300

    death modified

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,11 @@
 /*   By: kmeeseek <kmeeseek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/14 22:14:38 by kmeeseek          #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2021/07/24 15:29:25 by kmeeseek         ###   ########.fr       */
+=======
+/*   Updated: 2021/07/22 23:20:44 by kmeeseek         ###   ########.fr       */
+>>>>>>> parent of df5c085... death works incorrectly
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -132,10 +136,10 @@ int	create_mutexes_and_ph(t_all *all)
 		all->ph[i].right_fork = &all->forks[(i + 1) %  all->param.num_of_ph];
 		all->ph[i].message = &all->message;
 		all->ph[i].id = i + 1;
+		// printf("id = %i\n", all->ph[i].id);
 		all->ph[i].param = all->param;
 		all->ph[i].start_time = start_time;
 		all->ph[i].last_eat_time = 0;
-		all->ph[i].dead = (all->dead);
 		i++;
 	}
 	return (0);
@@ -145,15 +149,19 @@ void eating(t_ph *ph)
 {
 	long	curr_time;
 
+	// printf("here\n");
 	pthread_mutex_lock(ph->left_fork);
 	pthread_mutex_lock(ph->right_fork);
 
 	curr_time = (get_time() - ph->start_time);
+<<<<<<< HEAD
 	if(*(ph->dead))
 	{
 		// printf ("dead = %i\n", *(ph->dead));
 		return ;
 	}
+=======
+>>>>>>> parent of df5c085... death works incorrectly
 	pthread_mutex_lock(ph->message);
 	printf("%li %d has taken a fork\n", curr_time, ph->id);
 	printf("%li %d has taken a fork\n", curr_time , ph->id);
@@ -174,11 +182,14 @@ void sleeping(t_ph *ph)
 	long	curr_time;
 
 	curr_time = (get_time() - ph->start_time);
+<<<<<<< HEAD
 	if(*(ph->dead))
 	{
 		// printf ("dead = %i\n", *(ph->dead));
 		return ;
 	}
+=======
+>>>>>>> parent of df5c085... death works incorrectly
 	pthread_mutex_lock(ph->message);
 	// printf("%li %d is sleeping\n", ph->last_eat_time / 1000, ph->id);
 	printf("%li %d is sleeping\n", curr_time, ph->id);
@@ -192,18 +203,22 @@ void thinking(t_ph *ph)
 	long	curr_time;
 
 	curr_time = (get_time() - ph->start_time);
+<<<<<<< HEAD
 	if(*(ph->dead))
 	{
 		// printf ("dead = %i\n", *(ph->dead));
 		return ;
 	}
+=======
+>>>>>>> parent of df5c085... death works incorrectly
 	pthread_mutex_lock(ph->message);
 	printf("%li %d is thinking\n", curr_time , ph->id);
 	pthread_mutex_unlock(ph->message);
 }
 
-void	*action(void *arg)
+void *action(void *arg)
 {
+<<<<<<< HEAD
 	t_ph	*ph;
 	int		i;
 	long	death_time;
@@ -224,16 +239,27 @@ void	*action(void *arg)
 			printf("%li %d died\n", death_time , i + 1);
 			return(0);
 		}
+=======
+	// printf("here2\n");
+	t_ph *ph;
+
+	ph = (t_ph *)arg;
+	if (ph->id % 2 == 0)
+		usleep (1000);
+	// printf("ph.id = %i\n", ph->id);
+	while (1)
+	{
+>>>>>>> parent of df5c085... death works incorrectly
 		eating(ph);
 		sleeping(ph);
 		thinking(ph);
-		i++;
 	}
 	return (0);
 }
 
-void	*if_dead(void *arg)
+void	if_dead(t_all *all)
 {
+<<<<<<< HEAD
 	t_all	*all;
 	// int		i;
 	int		j;
@@ -279,38 +305,57 @@ void	*if_dead(void *arg)
 		}
 	// }
 	return (0);
-}
-
-int	create_treads(t_all *all)
-{
-	int	i;
+=======
+	int i;
 
 	i = 0;
 	while (i < all->param.num_of_ph)
 	{
+		if ((get_time() - all->ph->start_time - all->ph[i].last_eat_time) > all->param.time_to_die)
+		{
+			pthread_mutex_lock(&all->message);
+			printf("%li %d died\n", (get_time() - all->ph->start_time) , i + 1);
+			pthread_mutex_unlock(&all->message);
+			exit (1);
+		}
+		i++;
+	}
+	// return (0);
+>>>>>>> parent of df5c085... death works incorrectly
+}
+
+int	create_treads(t_all *all)
+{
+	int i;
+
+	i = 0;
+	// printf("here1\n");
+	while (i < all->param.num_of_ph)
+	{
+		// printf("here3\n");
 		pthread_create(&all->ph[i].philo, NULL, action, &all->ph[i]);
 		i++;
+<<<<<<< HEAD
 		// my_sleep(1);
+=======
+		// printf("here4\n");
+>>>>>>> parent of df5c085... death works incorrectly
 	}
 	i = 0;
-	// usleep (1000);
-	pthread_create(&all->death, NULL, if_dead, all);
-	pthread_join(all->death, NULL);
-	// if(all->dead == 1)
-	// {
-	// 	while (i < all->param.num_of_ph)
-	// 	{
-	// 		pthread_detach(all->ph[i].philo);
-	// 		i++;
-	// 	}
-	// 	return (0);
-	// }
+	if_dead(all);
+	{
+		while (i < all->param.num_of_ph)
+		{
+			pthread_detach(all->ph[i].philo);
+			i++;
+		}
+		return (1);
+	}
 	while (i < all->param.num_of_ph)
 	{
 		pthread_join(all->ph[i].philo, NULL);
 		i++;
 	}
-
 	i = 0;
 	return (0);
 }
@@ -325,5 +370,7 @@ int	main(int argc, char **argv)
 		return (1);
 	if (create_treads(&all))
 		return (1);
+	
+	
 	return (0);
 }
